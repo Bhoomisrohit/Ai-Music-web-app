7718915e050a1e5921f01e08390da1a7edf67c1b
BHOOMI ROHIT <93781609+Bhoomisrohit@users.noreply.github.com>
2022-05-10 18:51:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ leftWrist_X=0;
 leftWrist_Y=0;
 rightWrist_X=0;
 rightWrist_Y=0;
+leftWrist_score=0;
+rightWrist_score=0;
+song_status="";
 
 function preload()
 {
@@ -19,7 +22,7 @@ function setup()
     video=createCapture(VIDEO);
     video.hide();
 
-    poseNet=ml5.posenet(video,modelLoaded);
+    poseNet=ml5.poseNet(video,modelLoaded);
     poseNet.on("pose",gotPoses);
 
 }
@@ -41,12 +44,28 @@ function gotPoses(results)
     rightWrist_X=results[0].pose.rightWrist.x;
     rightWrist_Y=results[0].pose.rightWrist.y;
     console.log("rightWrist_X-" + rightWrist_X + "rightWrist_Y-" + rightWrist_Y );
+    leftWrist_score=results[0].pose.keypoints[9].score;
+    console.log("leftWrist score is- " + score_leftWrist); 
+    rightWrist_score=results[0].pose.keypoints[10].score;
+    console.log("rightWrist score is- " + score_rightWrist); 
+  
+  
   }
+
 }
 
 function draw()
 {
   image(video,0,0,600,500);
+  song_1.isPlaying();
+  stroke("red");
+  fill("red");
+if(leftWrist_score>0.2)
+  {
+    circle(300,400,20);
+    song_2.stop();
+
+  }
 }
 
 function play()
